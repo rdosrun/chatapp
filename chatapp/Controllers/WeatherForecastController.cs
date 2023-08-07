43d8b9784be64ec22dbba28e7bcdb200b1bb114d
rdosrun <rdosrun@gmail.com>
2023-08-06 21:22:06 -0600

    finished C# backend,added sql support,finished login html/css/javascript

--- chatapp/Controllers/WeatherForecastController.cs
+++ chatapp/Controllers/WeatherForecastController.cs
@@ -4,12 +4,15 @@ using System.IO;   // for MemoryStream
 using System.Net.Http; // for HttpResponseMessage
 using System.Net;  // for HttpStatusCode
 using System.Net.Http.Headers;  // for MediaTypeHeaderValue
+using System.Data;
+using MySqlConnector;
 namespace chatapp.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -21,6 +24,45 @@ public class WeatherForecastController : ControllerBase
     {
         _logger = logger;
     }
+
+    //sql connection
+    // set these values correctly for your database server
+    private DataTable Run_SQL(string command){
+        string cs = "server=127.0.0.1;userid=root;password=password;database=CHATAPP";
+        int id =-1;
+        MySqlDataReader reader = null;
+        DataTable table1 = new DataTable("values");
+        table1.Columns.Add("type");
+        table1.Columns.Add("value");
+        using (MySqlConnection connection = new MySqlConnection(cs)){
+            try
+            {
+                connection.Open();
+                using (MySqlCommand cmd = new MySqlCommand(command, connection))
+                {
+                    using (reader = cmd.ExecuteReader()){
+                        while( reader.Read()){
+                            table1.Rows.Add("int",reader.GetInt32(0));
+                            table1.Rows.Add("string",reader.GetString(1));
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        return table1;
+    }
+    }
+    // create a DB command and set the SQL statement with parameters
+
+
     private String getfile(string path){
         String tmp = "";
         String results ="";
@@ -64,5 +106,34 @@ public class WeatherForecastController : ControllerBase
        return Content (getfile("./html/index.css"),"text/css");
     }
 
+    [Route("Login")]
+    [HttpGet]
+    public async Task<ContentResult> SignIn(int ID,string password)
+    {
+        string cmd = "Select * from Login";
+        string psswd = "";
+        DataTable reader = Run_SQL(cmd);
+        foreach (DataRow item in reader.Rows)
+        {
+            psswd = item["value"].ToString();
+        }
+        if(psswd != password){
+            return Content(getfile("./html/landingpage.html"),"text/html");
+        }else{
+            return Content (getfile("./html/index.html"),"text/html");
+        }
+    }
+
+    [HttpGet("DBtest")]
+    public async Task<ContentResult> GetID(){
+        string cmd = "Select * from Users";
+        string id ="-1";
+        DataTable reader = Run_SQL(cmd);
+        foreach (DataRow item in reader.Rows)
+        {
+            id = item["value"].ToString();
+        }
+        return Content(id,"text/html");
+    }
 
 }
